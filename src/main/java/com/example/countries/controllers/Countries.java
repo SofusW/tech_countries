105fb431b8d7c8f91f3b8453ee9bcb7b0c902376
sofuswinther <sfswnthr@gmail.com>
2025-10-03 11:27:19 +0200

    beautiful changes

--- src/main/java/com/example/countries/controllers/Countries.java
+++ src/main/java/com/example/countries/controllers/Countries.java
@@ -1,14 +1,14 @@
 package com.example.countries.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class Countries {
 
     @GetMapping("/countries")
         public String getCountries() {
-        return "no";
+        return "home/countries";
     }
 
 }
